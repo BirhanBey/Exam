deffce61f37a8edbe018e6c4517f1a46ddc4cfcb
Birhan Yorukoglu <birhanyorukoglu@gmail.com>
2022-11-15 16:28:19 +0100

    oefening_1

---
+++ Oefening_1.js
@@ -0,0 +1,108 @@
+var rl = require("readline-sync");
+
+// let returnMessage = "";
+// const name = rl.question("what is yor name? ");
+// const surname = rl.question("What is your surname? ");
+// const address = rl.question("What is your address adn box number? ");
+// const postcode = rl.question("What is your postcode? ");
+// var municipality = rl.question('What is municipality ');
+
+// let barLine = "";
+// let lastName,lastSurname = "";
+
+// if (address.length > 40){
+//     returnMessage = "Kader is te breed. Gebruik afkortingen in de breedste stukken."
+// }
+// if ( postcode < 1000 || postcode >9900){
+//     returnMessage =  "Minstens één van de ingevulde velden is niet geldig!"
+// }
+// if( name.length == 0 && surname.length == 0){
+//     returnMessage = "Minstens één van de ingevulde velden is niet geldig!"
+// }else {
+//     lastName = name[0].toUpperCase()+'.';
+//     lastSurname = surname[0].toUpperCase() + surname.substring(1);
+// }
+
+// returnMessage = barLine+"\n |  "+ lastName+" "+lastSurname+'\n |'+ address+'\n |'+postcode+" "+municipality+"\n"+barLine;
+
+function Oefening1() {
+  const name = rl.question("what is yor name? ");
+  const surname = rl.question("What is your surname? ");
+  const address = rl.question("What is your address adn box number? ");
+  const postcode = rl.question("What is your postcode? ");
+  const municipality = rl.question("What is municipality ");
+
+  if (
+    name.length == 0 ||
+    surname.length == 0 ||
+    address.length == 0 ||
+    postcode.length == 0 ||
+    municipality.length == 0
+  ) {
+    return "Minstens één van de ingevulde velden is niet geldig!";
+  } else if (
+    name.length > 40 ||
+    surname.length > 40 ||
+    address.length > 40 ||
+    postcode.length > 40 ||
+    municipality.length > 40
+  ) {
+    return "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+  } else if (postcode < 1000 || postcode > 9900) {
+    return "Minstens één van de ingevulde velden is niet geldig!";
+  } else {
+    let topBottomLine = "";
+    let line01 = "";
+    let line02 = "";
+    let line03 = "";
+    let lastName = "";
+    let lastSurname = "";
+
+    lastName = name[0].toUpperCase() + ".";
+    lastSurname = surname[0].toUpperCase() + surname.substring(1);
+    let lastNameSurname = lastName + " " + lastSurname;
+
+    for (let i = 0; i < 44 - lastNameSurname.length; i++) {
+      if (i == 2) {
+        line01 += lastNameSurname;
+      } else {
+        line01 += " ";
+      }
+    }
+    for (let i = 0; i < 44 - address.length; i++) {
+      if (i == 2) {
+        line02 += address;
+      } else {
+        line02 += " ";
+      }
+    }
+    const pgSum = postcode.length + municipality.length + 1;
+
+    for (let i = 0; i < 44 - pgSum.length; i++) {
+      if (i == 2) {
+        line03 += postcode+" "+municipality;
+      } else {
+        line03 += " ";
+      }
+    }
+    for (let i = 0; i < line01.length; i++) {
+      topBottomLine += "-";
+    }
+    
+    return (
+      topBottomLine +
+      "\n|" +
+      line01 +
+      "|" +
+      "\n|" +
+      line02 +
+      "|" +
+      "\n| " +
+      line03 +
+      " |\n" +
+      topBottomLine
+    );
+  }
+}
+
+console.log(Oefening1());
---
+++ Oefening_2.js
@@ -0,0 +1,55 @@
+var rl = require("readline-sync");
+
+function Oefening(sentence, size) {
+  this.sentence = sentence;
+  this.size = size;
+}
+
+Oefening.prototype.left = function () {
+  return this.sentence.substr(0,this.size)
+};
+Oefening.prototype.right = function () {
+    return this.sentence.substr(-this.size, this.sentence.length)
+  };
+Oefening.prototype.charCount = function () {
+    return this.sentence.length;
+  };
+Oefening.prototype.wordCount = function () {
+    return this.sentence.split(" ").length;
+  };
+Oefening.prototype.vowelCount = function () {
+    const vowels = ['i',"a","e","u","o"];
+    const vowelSplit = this.sentence.split('');
+    let vowelsCount = 0;
+    vowelSplit.forEach((el) => {
+        for (let i = 0; i < vowels.length - 1; i++) {
+            if (el === vowels[i]) {
+                vowelsCount++;
+            }
+        }
+    });
+    return vowelsCount;
+};
+Oefening.prototype.capitalsCount = function () {
+    let toUpperSize = 0;
+    var parcalar = this.sentence.split(" ");
+    for (let i = 0; i < parcalar.length - 1; i++) {
+        if (parcalar[i].toUpperCase()) {
+            toUpperSize++;
+        }
+    }
+    return toUpperSize;
+};
+Oefening.prototype.rightAppend = function (x) {
+    return this.sentence+x;
+};
+Oefening.prototype.leftAppend = function (x) {
+    return x+this.sentence;
+};
+oneSentence = new Oefening(
+  "afisdfkiaşsdkf aişsldfaişdslk iasldkfialsd aişsldk faişlws",
+  10
+);
+
+console.log(oneSentence.rightAppend(" sonu"));
+console.log(oneSentence.leftAppend("başı"));
